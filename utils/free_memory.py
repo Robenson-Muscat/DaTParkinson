3344da573a9ee4a6e05ef2bfcc630f68609724c5
Robenson Mondelice <robensonmondelice@gmail.com>
2026-08-13 09:43:38 +0200

    Add utils functions

---
+++ utils/free_memory.py
@@ -0,0 +1,11 @@
+
+import gc
+import torch
+
+def free_gpu():
+    """After deleting huge variables, free collect garbage and CUDA cache"""
+
+    torch.cuda.synchronize()
+
+    gc.collect()
+    torch.cuda.empty_cache()
---
+++ utils/seed.py
@@ -0,0 +1,19 @@
+import torch
+import random
+import numpy as np
+
+
+def seed_everything(seed=26):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+    #torch.backends.cudnn.deterministic = True
+    #torch.backends.cudnn.benchmark = False
+
+
+#seed_everything(SEED)
