0da4a1d5149bbd791a4bb1070b6bcba22bb6df4e
Robenson Mondelice <robensonmondelice@gmail.com>
2026-08-13 08:39:55 +0200

    Add specific metrics including LogLoss

---
+++ metrics.py
@@ -0,0 +1,30 @@
+from sklearn.metrics import (
+    roc_auc_score,
+    log_loss
+)
+
+
+def compute_metrics(
+    y_true,
+    y_prob
+):
+
+    y_true = np.array(y_true)
+    y_prob = np.array(y_prob)
+
+
+    auc = roc_auc_score(
+        y_true,
+        y_prob
+    )
+
+
+    loss = log_loss(
+        y_true,
+        y_prob,
+        labels=[0,1]
+    )
+
+
+    return loss, auc
+
